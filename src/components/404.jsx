03884dc73744e1a76bdec47de3b75d1f1222d4d0
Amey <74102654+AmeyVijeesh@users.noreply.github.com>
2023-11-25 16:29:18 +0530

    Create 404.jsx

---
+++ src/components/404.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import img from "./images/dog1.png";
+import { NavLink } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
+      <div>
+        <img src={img} alt="" style={{ height: "200px", width: "200px" }} />
+        <h3>
+          Oops! Looks like you got lost. <NavLink>Go back home</NavLink>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
