1fc46070ec32affa7810712185db50c5a456ece6
Amey <74102654+AmeyVijeesh@users.noreply.github.com>
2023-11-25 16:56:26 +0530

    Create contact.jsx

---
+++ src/components/contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Contact</h2>
+      <p style={{ textAlign: "center" }}>
+        Contact me at my <a href="https://ameyvijeesh.netlify.com">website </a>
+        or my <a href="https://github.com/AmeyVijeesh">Github.</a>
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
