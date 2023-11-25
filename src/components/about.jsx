10d8d80c60596d2dfae107988694882cbf58d738
Amey <74102654+AmeyVijeesh@users.noreply.github.com>
2023-11-25 16:19:18 +0530

    Create about.jsx

---
+++ src/components/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This website was made by Amey.{" "}
+      <a href="https://ameyvijeesh.netlify.com">Website</a>
+    </div>
+  );
+};
+
+export default About;
