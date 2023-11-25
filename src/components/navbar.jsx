de56d8f5d3bed6a13c25a973e827fd11915607c6
Amey <74102654+AmeyVijeesh@users.noreply.github.com>
2023-11-25 16:18:56 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, NavLink } from "react-router-dom";
+import { useNavigate, NavLink, Link } from "react-router-dom";
 import { signOut, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from "react";
 
@@ -7,20 +7,16 @@ const Navbar = ({ auth }) => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
-  // Use useEffect to listen to changes in the user's authentication status
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is logged in
         setUser(user);
       } else {
-        // User is logged out
         setUser(null);
       }
     });
 
     return () => {
-      // Unsubscribe the listener when the component unmounts
       unsubscribe();
     };
   }, [auth]);
@@ -69,48 +65,21 @@ const Navbar = ({ auth }) => {
             <div class="navbar-collapse collapse" id="mobile_menu">
               <ul class="nav navbar-nav">
                 <li>
-                  <a href="">
-                    <NavLink to="/home">Home</NavLink>
-                  </a>
+                  <Link to="/home" style={{ color: "#fff" }}>
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    About <span class="caret"></span>
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a href="#">About AmExpro</a>
-                    </li>
-                    <li>
-                      <a href="#">Legacy</a>
-                    </li>
-                    <li>
-                      <a href="#">About the Creator</a>
-                    </li>
-                    <li>
-                      <a href="#">About Four</a>
-                    </li>
-                    <li>
-                      <a href="#">About Five</a>
-                    </li>
-                    <li>
-                      <a href="#">About Six</a>
-                    </li>
-                  </ul>
+                  <a href="/about">About</a>
                 </li>
                 <li>
-                  <a href="#">
-                    <NavLink to="/items">Items</NavLink>
-                  </a>
+                  <Link to="/items" style={{ color: "#fff" }}>
+                    Items
+                  </Link>
                 </li>
+
                 <li>
-                  <a href="#">Services</a>
-                </li>
-                <li>
-                  <a href="#">Gallery</a>
-                </li>
-                <li>
-                  <a href="#">Contact Us</a>
+                  <a href="#">Contact</a>
                 </li>
               </ul>
               <ul class="nav navbar-nav">
@@ -125,7 +94,7 @@ const Navbar = ({ auth }) => {
                 <li>
                   <a href="#">
                     <span class="glyphicon glyphicon-shopping-cart"></span>{" "}
-                    <NavLink to="/cart">Cart</NavLink>
+                    <Link to="/cart">Cart</Link>
                   </a>
                 </li>
                 <li>
@@ -135,20 +104,18 @@ const Navbar = ({ auth }) => {
                 </li>
                 <li>
                   {user ? (
-                    // If user is logged in, show the "Logout" link
                     <a href="#" onClick={onLogOut}>
                       <span class="glyphicon glyphicon-log-out"></span> Logout
                     </a>
                   ) : (
-                    // If user is not logged in, show the "Login" link
                     <a href="#">
                       <span
                         class="glyphicon glyphicon-log-in"
                         style={{ color: "white" }}
                       ></span>{" "}
-                      <NavLink style={{ color: "white" }} to="/login">
+                      <Link style={{ color: "white" }} to="/login">
                         Login
-                      </NavLink>
+                      </Link>
                     </a>
                   )}
                 </li>
